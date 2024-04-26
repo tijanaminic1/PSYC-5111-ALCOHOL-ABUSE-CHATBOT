c778378ffbc7b642678a8862043a34efa1028801
tijanaminic1 <63131835+tijanaminic1@users.noreply.github.com>
2024-04-26 19:14:36 -0400

    updateee

--- AA-app.py
+++ AA-app.py
@@ -29,31 +29,32 @@ questions = [
     "Do you engage in pregaming? (yes/no)",
     "Do you participate in Greek life? (yes/no)",
     "Do you live on campus?(yes/no)",
-    "Please reply to the following statement with yes/no:\n I am comfortable reaching out to my RA for guidance/help.",
-    "Please reply to the following statement with yes/no:\n When I go to parties that involve drinking, I make sure to bring friends that I trust will take care of me",
-    "Please reply to the following statement with yes/no: \nI only go to frat parties that I heard from friends were safe and have a good risk-prevention team",
-    "Please reply to the following statement with yes/no:\n When I go to parties that involve drinking, I make sure to have a ride home from a sober friend/person.",
-    "Please reply to the following statement with yes/no:\n At parties, I never leave my drink unattended.",
-    "Please reply to the following statement with yes/no: \nI usually avoid mixing my drinks"
+    "I am comfortable reaching out to my RA for guidance/help. (yes/no)",
+    "When I go to parties that involve drinking, I make sure to bring friends\n that I trust will take care of me. (yes/no)",
+    "I only go to frat parties that I heard from friends were safe and have\n a good risk-prevention team. (yes/no)",
+    "When I go to parties that involve drinking, I make sure to have a ride\n home from a sober friend/person. (yes/no)",
+    "At parties, I never leave my drink unattended. (yes/no)",
+    "I usually avoid mixing my drinks. (yes/no)"
 ]
 current_question = 0
 points = 0
 triggers = []
 
-BMI = "Brief Motivational Interviewing (BMI) would involve you meeting one-on-one with an interventionist whose there to help you by changing your motivates around the problem. Additionally, it would involve getting personalized feedback from your interventionist whilst learning more about alcohol. \n\n"
-AMI = "Adaptations of Motivational Interviewing (AMI) would involve you meeting with a counselor to help build the motivation to make changes to your life about a specific issue. You would meet them and they would be non-judgmental, empathetic, understanding, and supporting you to make the changes you want to make about your specific circumstances. AMI would also incorporate a few other elements besides the basis of Motivational Interviewing (MI) but ultimately it is to help change motivations to make changes when you are having difficulty making the changes you want to make.\n\n"
-EI = "An Educational Intervention would help you to learn a greater deal about alcohol and alcohol within the college environment. They would help you to understand more about the challenges provided by alcohol and how to have safe behaviors if you do drink.\n\n"
-ECS = " Expectancy Challenges would help teach you the actual effects of alcohol on a person. The goal is to train you out of current expectations you may have about someone’s behavior and how they are affected after drinking and learn what those behaviors and effects really are. The goal is to challenge current expectations and set in your mind proper expectations for what drinking does.\n\n"
-# ASTP = 
-DRT = "Deviance Regulation Theory would help in changing how you view the norms of your surrounding environment, especially in terms of healthy or unhealthy drinking behaviors. If you tend to think that unhealthy behaviors are more common in your environment, then you will receive messages promoting and encouraging healthy behaviors. If you believe that the behaviors around you tend to be healthier, then you will receive messages that discourage unhealthy drinking behaviors. \n\n"
-PBS = "Protective Behavioral Strategies would teach you behaviors and actions to take while drinking that would reduce negative consequences from drinking. Overall, the rate and severity of negative consequences from drinking alcohol would be reduced through learning these strategies. \n\n"
-# BASICS =
-PACE = "Pregaming Awareness in College Environments would be a set of modules occurring online that would discuss the high-risks of pregaming and its consequences. The goal would be to reduce pregaming behaviors and therefore any negative consequences that may occur from it. \n\n"
-GLF = "Useful tip: if you're visiting parties hosted by Greek life organizations, it would be good to always go with a group of friends you trust to take care of you and be respondible.\n\n"
-GLR = "Useful tip: try to do your research on different Greek life organizations before you visit their parties. Specifically, target the ones known for their good risk prevention team, as they will take good care of you if anything bad happens! \n\n"
-RA = "Sometimes reaching out to your Residents Assistant (RA) can help finding resources if you're dealing with alcohol abuse problems. They are closer to you in age than your professors, so they can find a way to help you without making you feel ashamed or uncomfortable!\n\n"
-CAPS = "Seton Hall University's Counseling and Psychological Services (CAPS) is an easily accessible intervention that students can utilize. They can schedule a meeting with campus by contacting them or coming into the office. CAPS can greatly assist students dealing with smaller issues regarding alcohol abuse and you can even have weekly or monthly meetings with them. Those with greater issues can get a referral to a professional who would be more tailored to help them. \n\n"
-
+BMI = "Brief Motivational Interviewing (BMI) would involve you meeting one-on-one with \n an interventionist whose there to help you by changing your motivates around the \n problem. Additionally, it would involve getting personalized feedback \n from your interventionist whilst learning more about alcohol. \n\n"
+AMI = "Adaptations of Motivational Interviewing (AMI) would involve you meeting with a \n counselor to help build the motivation to make changes to your life about a \n specific issue. You would meet them and they would be non-judgmental, empathetic,\n understanding, and supporting you to make the changes you want to make about your\n specific circumstances. AMI would also incorporate a few other elements besides\n the basis of Motivational Interviewing (MI) but ultimately it is to help change\n motivations to make changes when you are having difficulty making the\n changes you want to make.\n\n"
+EI = "An Educational Intervention would help you to learn a greater deal about alcohol \n and alcohol within the college environment. They would help you to understand more \n about the challenges provided by alcohol and how to have safe behaviors \nif you do drink.\n\n"
+ECS = " Expectancy Challenges would help teach you the actual effects of alcohol on a \n person. The goal is to train you out of current expectations you may have about\n someone's behavior and how they are affected after drinking and learn what those\n behaviors and effects really are. The goal is to challenge current \nexpectations and set in your mind proper expectations for what drinking does.\n\n"
+ASTP = "In CAPS, ASTP stands for Alcohol Skills Training Program. This would entail a \n 90-minute session bent on helping students to develop a bettter understanding of\n what addiction is and how it works. Additionally, ASTP would focus on supporting\n students to develop necessary skills in order to decline alcohol and understand\n drinking amounts."
+DRT = "Deviance Regulation Theory would help in changing how you view the norms of your\n surrounding environment, especially in terms of healthy or unhealthy \ndrinking behaviors. If you tend to think that unhealthy behaviors \nare more common in your environment, then you will receive\n messages promoting and encouraging healthy behaviors. If you believe \nthat the behaviors around you tend to be healthier, then you will receive \nmessages that discourage unhealthy drinking behaviors. \n\n"
+PBS = "Protective Behavioral Strategies would teach you behaviors and actions to take \n while drinking that would reduce negative consequences from drinking. \nOverall, the rate and severity of negative consequences from drinking\n alcohol would be reduced through learning these strategies. \n\n"
+PACE = "Pregaming Awareness in College Environments would be a set of modules occurring\n online that would discuss the high-risks of pregaming and its consequences.\n The goal would be to reduce pregaming behaviors and therefore any negative \nconsequences that may occur from it. \n\n"
+GLF = "Useful tip: if you're visiting parties hosted by Greek life organizations,\n it would be good to always go with a group of friends you trust to take\n care of you and be respondible.\n\n"
+GLR = "Useful tip: try to do your research on different Greek life organizations before\n you visit their parties. Specifically, target the ones known for their good \nrisk prevention team, as they will take good care of you if anything\n bad happens! \n\n"
+RA = "Sometimes reaching out to your Residents Assistant (RA) can help finding resources\n if you're dealing with alcohol abuse problems. They are closer to you in \nage than your professors, so they can find a way to help you without \nmaking you feel ashamed or uncomfortable!\n\n"
+CAPS = "Seton Hall University's Counseling and Psychological Services (CAPS) is an easily \n accessible intervention that students can utilize. They can schedule a \n meeting with campus by contacting them or coming into the office. CAPS can greatly\n assist students dealing with smaller issues regarding alcohol abuse and you \ncan even have weekly or monthly meetings with them. Those with greater issues can get a referral\n to a professional who would be more tailored to help them. \n\n"
+AEFT = "Academic Goal-Relevant Episodic Future Thinking or A-EFT would involve you trying\n to see the future of taking a certain action. A-EFT would work on your \nability to see the results of taking a particular action.\n Additionally, A-EFT would encourage you to view events that had positive\n impacts on your academic goal both in a short-term and in a long-term \nmanner to help you better understand the impacts your actions have in\n the outcomes you create. By helping you to develop a better ability\n to predict the future based on your actions it is trying to\n help with managing impulses, or having better control over yourself.\n\n"
+MHH = "This 973-275-4357 is the number you should reach out to if you are having\n a mental health crisis. Someone will be able to give you immediate help\n in whatever situation is troubling you.\n\n"
+AUDIT = "Alcohol Use Disorders Identification Test is a screening tool designed to assess you\n for unhealthy alcohol use. It is a simple and effective way to anonymously find out for\n yourself if you may have any issues with alcohol.\n\n"
 
 def print_trigger_text(triggers):
     trigger_texts = {
@@ -97,6 +98,7 @@ def send():
 
         except ValueError:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter a valid age.")
+            current_question -= 1
 
     elif current_question == 1:
         try:
@@ -111,6 +113,7 @@ def send():
                 triggers.extend(["AMI", "BMI", "EI", "ECS"])
         except ValueError:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter a valid number.")
+            current_question -= 1
 
     elif current_question == 2:
         if user_input.lower() == "na":
@@ -125,6 +128,7 @@ def send():
                     points += 5
             except ValueError:
                 txt.insert(tk.END, "\n" + "Sippy -> Please enter a valid age or NA.")
+                current_question -= 1
 
     elif current_question == 3:
         if user_input.lower() == "yes":
@@ -134,6 +138,7 @@ def send():
             pass  # No points for no family history
         else:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter either 'yes' or 'no'.")
+            current_question -= 1
 
     elif current_question == 4:
         try:
@@ -146,6 +151,7 @@ def send():
                 points += 5
         except ValueError:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter a valid number.")
+            current_question -= 1
 
     elif current_question == 5:
         try:
@@ -161,6 +167,7 @@ def send():
                 triggers.extend(["PBS", "DRT"])
         except ValueError:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter a valid number.")
+            current_question -= 1
 
     elif current_question == 6:
         try:
@@ -181,6 +188,7 @@ def send():
                 triggers.append("DRT")
         except ValueError:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter a valid number.")
+            current_question -= 1
 
     elif current_question == 7:
         try:
@@ -199,6 +207,7 @@ def send():
                 triggers.extend(["EI", "PBS", "BMI"])
         except ValueError:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter a valid number.")
+            current_question -= 1
 
     elif current_question == 8:
         try:
@@ -219,6 +228,7 @@ def send():
                 triggers.append("EI")
         except ValueError:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter a valid number.")
+            current_question -= 1
 
     elif current_question == 9:
         if user_input.lower() == "yes":
@@ -228,6 +238,7 @@ def send():
             points -= 10
         else:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter either 'yes' or 'no'.")
+            current_question -= 1
 
     elif current_question == 10:
         if user_input.lower() == "yes":
@@ -236,6 +247,7 @@ def send():
             points -= 10
         else:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter either 'yes' or 'no'.")
+            current_question -= 1
 
     elif current_question == 11:
         if user_input.lower() == "yes":
@@ -244,6 +256,7 @@ def send():
             pass
         else:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter either 'yes' or 'no'.")
+            current_question -= 1
 
     elif current_question == 12:
         if user_input.lower() == "yes":
@@ -252,6 +265,7 @@ def send():
             points -= 5
         else:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter either 'yes' or 'no'.")
+            current_question -= 1
 
     elif current_question == 13:
         if user_input.lower() == "yes":
@@ -260,6 +274,7 @@ def send():
             points -= 5
         else:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter either 'yes' or 'no'.")
+            current_question -= 1
 
     elif current_question == 14:
         if user_input.lower() == "yes":
@@ -268,6 +283,7 @@ def send():
             points -= 5
         else:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter either 'yes' or 'no'.")
+            current_question -= 1
 
     elif current_question == 15:
         if user_input.lower() == "yes":
@@ -276,6 +292,7 @@ def send():
             points -= 5
         else:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter either 'yes' or 'no'.")
+            current_question -= 1
 
     elif current_question == 16:
         if user_input.lower() == "yes":
@@ -284,25 +301,30 @@ def send():
             points -= 5
         else:
             txt.insert(tk.END, "\n" + "Sippy -> Please enter either 'yes' or 'no'.")
+            current_question -= 1
 
     current_question += 1
     if current_question < len(questions):
         txt.insert(tk.END, "\n" + "Sippy -> " + questions[current_question])
     else:
-    # Provide feedback based on the total score
-        if points >= 60:
-            txt.insert(tk.END, "\n\n\n" + "You might want to consider your drinking habits and seek help if needed.\n", "result")
-            print_trigger_text(list(set(triggers)))
+            # Provide feedback based on the total score
+            if points >= 60:
+                triggers.extend(["ASTP", "AUDIT", "AEFT", "EI", "CAPS","AMI", "MHH"])
+                txt.insert(tk.END, "\n\nYou might want to consider your drinking habits and seek help if needed", "result")
+                print_trigger_text(list(set(triggers)))
 
-        elif points >= 30:
-            txt.insert(tk.END, "\n\n\n" + "Your drinking habits seem relatively moderate.\n", "result")
-            print_trigger_text(list(set(triggers)))
-        else:
-            txt.insert(tk.END, "\n\n\n" + "You appear to have healthy drinking habits.\n", "result")
-            print_trigger_text(list(set(triggers)))
+            elif points >= 30:
+                triggers.extend(["AMI", "AEFT", "AUDIT", "CAPS", "MHH"])
+                txt.insert(tk.END, "\n\nYour drinking habits seem relatively moderate.", "result")
+                print_trigger_text(list(set(triggers)))
+            else:
+                triggers.extend(["AMI", "AUDIT", "CAPS", "AEFT", "MHH"])
+                txt.insert(tk.END, "\n\nYou appear to have healthy drinking habits.\n\n\n", "result")
+                print_trigger_text(list(set(triggers)))
 
-# Configure the font color
+                # Configure the font color
     txt.tag_config("result", foreground=RESULT_COLOR)
+
 
 
     e.delete(0, tk.END)
